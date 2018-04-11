e802601211e998ef8978e636d82e3a9b85dfa749
Riza Saputra <riza.saputra@2359media.com>
2018-04-12 00:33:37 +0700

    feat: find optimum tasks subset

--- task.js
+++ task.js
@@ -17,5 +17,53 @@ module.exports = {
     })
 
     return tasks
+  },
+
+  /**
+   * Find optimum tasks subset from an ordered tasks
+   * @param {array} tasks
+   * @param {integer} totalScore
+   */
+  optimumTasksSubset (tasks = [], totalScore = 0) {
+    let optimumTasks = []
+    let highScore = 0
+    let remainingTasksMaxScore = totalScore
+
+    while (tasks.length > 0 && remainingTasksMaxScore > highScore) {
+      const baseTask = tasks.shift()
+
+      const baseTaskCompatibles = tasks.reduce(
+        (result, currentTask, idx, arr) => {
+          const previousTask = idx === 0 ? baseTask : arr[idx - 1]
+
+          // no overlap
+          if (previousTask.endTime < currentTask.startTime) {
+            result.score += currentTask.score
+            result.nonOverlapTasks.push(currentTask)
+          } else {
+            if (previousTask.score < currentTask.score) {
+              result.score += currentTask.score - previousTask.score
+              result.nonOverlapTasks.pop()
+              result.nonOverlapTasks.push(currentTask)
+            }
+          }
+
+          return result
+        },
+        {
+          score: baseTask.score,
+          nonOverlapTasks: [baseTask]
+        }
+      )
+
+      if (baseTaskCompatibles.score > highScore) {
+        highScore = baseTaskCompatibles.score
+        optimumTasks = baseTaskCompatibles.nonOverlapTasks
+      }
+
+      remainingTasksMaxScore -= baseTask.score
+    }
+
+    return { optimumTasks, highScore }
   }
 }
--- test/task.test.js
+++ test/task.test.js
@@ -24,7 +24,7 @@ describe('Modify task', () => {
         }
       ]
 
-      sortedTasks = task
+      const sortedTasks = task
         .dateSort(randomTasks)
         .map(task => task.startTime.format())
       const expected = [
@@ -57,12 +57,204 @@ describe('Modify task', () => {
         }
       ]
 
-      sortedTasks = task
+      const sortedTasks = task
         .dateSort(orderedTasks)
         .map(task => task.startTime.format())
-      orderedTasksStartTime = orderedTasks.map(task => task.startTime.format())
+      const orderedTasksStartTime = orderedTasks.map(task =>
+        task.startTime.format()
+      )
 
       assert.sameOrderedMembers(sortedTasks, orderedTasksStartTime)
+    })
+  })
+
+  describe('find optimum tasks', () => {
+    it('return empty task if passed nothing', () => {
+      const result = task.optimumTasksSubset()
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 0)
+      assert.equal(result.highScore, 0)
+    })
+
+    it('return empty task if passed empty tasks list', () => {
+      const result = task.optimumTasksSubset([], 0)
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 0)
+      assert.equal(result.highScore, 0)
+    })
+
+    it('return the task if passed single task', () => {
+      const tasks = [
+        {
+          name: 'Task A',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 04:00'),
+          score: 100
+        }
+      ]
+      const result = task.optimumTasksSubset(tasks, 100)
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 1)
+      assert.equal(result.optimumTasks[0].name, 'Task A')
+      assert.equal(result.highScore, 100)
+    })
+
+    it('return all tasks and total tasks score if no time collision', () => {
+      const tasks = [
+        {
+          name: 'Task A',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 04:00'),
+          score: 100
+        },
+        {
+          name: 'Task B',
+          startTime: moment('2018-04-11 05:00'),
+          endTime: moment('2018-04-11 06:00'),
+          score: 200
+        },
+        {
+          name: 'Task C',
+          startTime: moment('2018-04-11 07:00'),
+          endTime: moment('2018-04-11 08:00'),
+          score: 300
+        },
+        {
+          name: 'Task D',
+          startTime: moment('2018-04-11 09:00'),
+          endTime: moment('2018-04-11 10:00'),
+          score: 400
+        }
+      ]
+      const result = task.optimumTasksSubset(tasks, 1000)
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 4)
+      assert.sameOrderedMembers(result.optimumTasks.map(task => task.name), [
+        'Task A',
+        'Task B',
+        'Task C',
+        'Task D'
+      ])
+      assert.equal(result.highScore, 1000)
+    })
+
+    it('return task with highest score if the all tasks time are collided', () => {
+      const tasks = [
+        {
+          name: 'Task A',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 04:00'),
+          score: 100
+        },
+        {
+          name: 'Task B',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 06:00'),
+          score: 200
+        },
+        {
+          name: 'Task C',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 08:00'),
+          score: 500
+        },
+        {
+          name: 'Task D',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 10:00'),
+          score: 400
+        }
+      ]
+      const result = task.optimumTasksSubset(tasks, 1200)
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 1)
+      assert.sameOrderedMembers(result.optimumTasks.map(task => task.name), [
+        'Task C'
+      ])
+      assert.equal(result.highScore, 500)
+    })
+
+    it('exclude task with lower score that listed first in time overlap case', () => {
+      const tasks = [
+        {
+          name: 'Task A',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 04:00'),
+          score: 100
+        },
+        {
+          name: 'Task B',
+          startTime: moment('2018-04-11 05:00'),
+          endTime: moment('2018-04-11 06:00'),
+          score: 200
+        },
+        {
+          name: 'Task C',
+          startTime: moment('2018-04-11 07:00'),
+          endTime: moment('2018-04-11 08:00'),
+          score: 300
+        },
+        {
+          name: 'Task D',
+          startTime: moment('2018-04-11 07:00'),
+          endTime: moment('2018-04-11 10:00'),
+          score: 400
+        }
+      ]
+      const result = task.optimumTasksSubset(tasks, 1000)
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 3)
+      assert.sameOrderedMembers(result.optimumTasks.map(task => task.name), [
+        'Task A',
+        'Task B',
+        'Task D'
+      ])
+      assert.equal(result.highScore, 700)
+    })
+
+    it('exclude task with lower score that listed later in time overlap case', () => {
+      const tasks = [
+        {
+          name: 'Task A',
+          startTime: moment('2018-04-11 03:00'),
+          endTime: moment('2018-04-11 04:00'),
+          score: 100
+        },
+        {
+          name: 'Task B',
+          startTime: moment('2018-04-11 05:00'),
+          endTime: moment('2018-04-11 06:00'),
+          score: 200
+        },
+        {
+          name: 'Task C',
+          startTime: moment('2018-04-11 07:00'),
+          endTime: moment('2018-04-11 08:00'),
+          score: 400
+        },
+        {
+          name: 'Task D',
+          startTime: moment('2018-04-11 07:00'),
+          endTime: moment('2018-04-11 10:00'),
+          score: 300
+        }
+      ]
+      const result = task.optimumTasksSubset(tasks, 1000)
+
+      assert.isArray(result.optimumTasks)
+      assert.equal(result.optimumTasks.length, 3)
+      assert.sameOrderedMembers(result.optimumTasks.map(task => task.name), [
+        'Task A',
+        'Task B',
+        'Task C'
+      ])
+      assert.equal(result.highScore, 700)
     })
   })
 })
